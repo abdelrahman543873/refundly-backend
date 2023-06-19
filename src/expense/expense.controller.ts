c8c9d3b9506841a23bbf62142932f3a3e4e749e7
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2023-06-19 19:36:09 +0200

    feat: added documentation for expense

--- src/expense/expense.controller.ts
+++ src/expense/expense.controller.ts
@@ -1,13 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ExpenseService } from './expense.service';
 import { AddExpenseDto } from './dtos/add-expense.dto';
+import { Expense } from './expense.entity';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('expense')
 @Controller('expense')
 export class ExpenseController {
   constructor(private readonly expenseService: ExpenseService) {}
 
   @Post()
-  async addExpense(@Body() addExpenseDto: AddExpenseDto) {
+  async addExpense(@Body() addExpenseDto: AddExpenseDto): Promise<Expense> {
     return await this.expenseService.addExpense(addExpenseDto);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MainValidationPipe } from './shared/pipes/main-validation.pipe';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('🚀refundly task')
+    .setDescription('refundly description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(MainValidationPipe);
   await app.listen(3000);
 }
