62ea9825cb739d8e51c634ef1cd4e7c3bbdc0382
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2023-07-03 19:51:54 +0200

    fix: user auth

--- src/expense/expense.controller.ts
+++ src/expense/expense.controller.ts
@@ -52,6 +52,7 @@ export class ExpenseController {
     return await this.expenseService.getExpenses(this.request.user.id);
   }
 
+  @ApiBearerAuth()
   @Get('summary')
   async summary() {
     return await this.expenseService.summary(this.request.user.id);
