0ce16cd572e5177ffb000a25ba0c1e3aacb29c65
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2023-07-06 18:47:21 +0200

    feat: added user info

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,12 @@
+import { RequestContext } from './../shared/interfaces/request-context.interface';
 import {
   Body,
   Controller,
+  Get,
+  Inject,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { UserService } from './user.service';
@@ -10,14 +14,25 @@ import { AuthDto } from './dtos/auth.dto';
 import { RegisterDto } from './dtos/register.dto';
 import { ApiConsumes } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { REQUEST } from '@nestjs/core';
+import { AuthGuard } from 'src/shared/guards/auth.guard';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    @Inject(REQUEST) private readonly request: RequestContext,
+  ) {}
 
   @Post('auth')
   async authenticateUser(@Body() authDto: AuthDto) {
     return await this.userService.authenticateUser(authDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('info')
+  async info() {
+    return this.request.user;
   }
 
   @ApiConsumes('multipart/form-data')
